b01dfde9553e7288f67508f8434a44c5cc560d3a
Adam Buczynski <adambuczynski@gmail.com>
2014-12-10 10:58:47 +1300

    Menu become transparent later

--- client/app/layout/nav/nav.ctrl.js
+++ client/app/layout/nav/nav.ctrl.js
@@ -14,7 +14,7 @@ angular.module('DigiSite.Layout.Nav.Controller', [
 	//Detect scrolling
 	$scope.isScrolling = false;
 	$scope.$on('detectedScrolling', function(event, element, offset) {
-		$scope.isScrolling = (offset > 96);
+		$scope.isScrolling = (offset > 128);
 	});
 	$scope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
 		$scope.isScrolling = false;
